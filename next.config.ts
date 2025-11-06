3988ca1cf7b66641c93d4df08e3c5aff25bec6ea
Aditya Kumbhar <adityakumbhar915@gmail.com>
2025-11-06 20:22:23 +0530

    updated turbopack error

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Prevent Prisma from being externalized to ensure binaries are bundled
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Include Prisma binaries in the serverless function bundle
-      config.externals = [...(config.externals || []), '_http_common'];
-    }
-    return config;
-  },
-  // Ensure output file tracing includes Prisma client files
-  outputFileTracingIncludes: {
-    '/api/**': ['./node_modules/.prisma/client/**/*'],
-  },
 };
 
 export default nextConfig;
-
-
